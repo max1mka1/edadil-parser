a2f30fe6602e6e783e2d5e6d93288afa39e51a36
max1mka1 <maxinstellar@gmail.com>
2020-08-15 23:18:45 +0300

    "Edadil parser get all goods for Chelyabinsk region"

--- main.py
+++ main.py
@@ -46,6 +46,10 @@ class EdadilParser():
     Класс EdadilParser()
     '''
 
+    data = {'description': [], 'categories': [], 'retailer': [],
+            'quantity': [], 'dates': [], 'new_price': [],
+            'old_price': [], 'discount': [],  'image': [], 'link': []}
+
     def __init__(self):
         '''
         Конструктор класса EdadilParser()
@@ -58,6 +62,11 @@ class EdadilParser():
         self.browser = webdriver.Chrome(executable_path=self.chromedriver_path, chrome_options=self.options)
         self.browser.implicitly_wait(10)  # seconds
 
+    def page_has_loaded(self):
+        # print(f"Checking if {self.browser.current_url} page is loaded")
+        page_state = self.browser.execute_script('return document.readyState;')
+        return page_state == 'complete'
+
     def parse_page(self, url):
         '''
         Метод parse_page() парсит страницу с конкретным товаром
@@ -68,31 +77,39 @@ class EdadilParser():
         try:
             # Переход на страницу входа
             self.browser.get(url)
-            sleep(1)
+            page_is_ready = self.page_has_loaded()
+            while page_is_ready != True:
+                page_is_ready = self.page_has_loaded()
+                sleep(0.3)
+                print(f'0.3')
             try:
                 retailer = self.browser.find_element_by_class_name("p-offer__retailer-title")
                 retailer = retailer.text
             except NoSuchElementException:
                 retailer = '-'
                 logging.exception(f'NoSuchElementException: retailer')
+                pass
             try:
                 dates = self.browser.find_element_by_class_name("p-offer__dates")
                 dates = dates.text
             except NoSuchElementException:
                 dates = '-'
                 logging.exception(f'NoSuchElementException: dates')
+                pass
             try:
                 image = self.browser.find_element_by_class_name("b-image__img")
                 image = image.get_attribute("src")
             except NoSuchElementException:
                 image = '-'
                 logging.exception(f'NoSuchElementException: image')
+                pass
             try:
                 description = self.browser.find_element_by_class_name("p-offer__description")
                 description = description.text
             except NoSuchElementException:
                 description = '-'
                 logging.exception(f'NoSuchElementException: description')
+                pass
             try:
                 new_price = self.browser.find_element_by_class_name("p-offer__price-new")
                 new_price = new_price.text
@@ -101,6 +118,7 @@ class EdadilParser():
             except NoSuchElementException:
                 new_price = '-'
                 logging.exception(f'NoSuchElementException: new_price')
+                pass
             old_price = self.browser.find_elements_by_class_name("p-offer__price-old")
             if len(old_price) == 0:
                 old_price = '-'
@@ -109,7 +127,7 @@ class EdadilParser():
                 old_price = old_price.text
                 old_price = old_price.replace(',', '.')
                 old_price = old_price.split()[0]
-                print(f"old_price = {old_price}")
+                # print(f"old_price = {old_price}")
             discount = self.browser.find_elements_by_class_name("p-offer__discount")
             if len(discount) == 0:
                 discount = '-'
@@ -125,16 +143,17 @@ class EdadilParser():
                     categories.append(category.text)
                 categories = ','.join(categories)
             try:
-                quantity = self.browser.find_element_by_class_name("p - offer__quantity")
+                quantity = self.browser.find_element_by_class_name("p-offer__quantity")
                 quantity = quantity.text
-                print(f"quantity = {quantity}")
+                # print(f"quantity = {quantity}")
             except:
                 quantity = '-'
+                pass
 
             # Можно также парсить похожие категории товаров
             # similar = self.browser.find_element_by_class_name("p-offer__similar-offers")
             dict_of_info = {'description': description, 'categories': categories, 'retailer': retailer,
-                            'dates': dates, 'new_price': new_price, 'old_price': old_price,
+                            'quantity': quantity, 'dates': dates, 'new_price': new_price, 'old_price': old_price,
                             'discount': discount, 'image': image, 'link': url}
             return dict_of_info
         except Exception as e:
@@ -188,6 +207,22 @@ class EdadilParser():
         print(f"File {name + '.txt'} sucsessfully saved!")
 
 
+    def create_databases(self, database, all_dict_of_info):
+        path = os.path.join(os.getcwd(), 'info_files')
+        database.to_excel(os.path.join(path, "Edadil_database.xlsx"), sheet_name='Sheet_name_1')
+        print(f"Database .xlsx created sucsessfully!")
+        database.to_csv(os.path.join(path, 'Edadil_database.csv'), sep=',')
+        print(f"Database .csv created sucsessfully!")
+        '''
+        with open("Edadil_database.txt", "w") as text_file:
+            for dict_ in all_dict_of_info:
+                for key, val in dict_.items():
+                    text_file.write(val)
+                    text_file.write(';')
+                text_file.write('\n')
+        print(f"Database .txt created sucsessfully!")
+        '''
+
 def main():
 
     parser = EdadilParser()
@@ -201,29 +236,28 @@ def main():
     print(f'Количество магазинов = {len(retailers_links)}')
 
     all_goods_links, all_pages_links = [], []
-    for retailer_link in tqdm(retailers_links):
+    for retailer_link in tqdm(retailers_links[8:9]):
         pages_links = parser.parse_url_for_classname(url=retailer_link, class_name=buttons_classname)
         pages_links = [link for link in pages_links if ((type(link) is not type(None)) and ('page=' in link))]
         all_pages_links.append(retailer_link)
         for page_link in pages_links:
             all_pages_links.append(page_link)
     print(f'Количество ссылок на все страницы магазинов = {len(all_pages_links)}')
+    parser.save_list_to_txt(all_pages_links, "all_pages_links")
 
     for page_link in tqdm(all_pages_links):
         goods_links_from_page_links = parser.parse_url_for_classname(url=page_link, class_name=goods_offers)
         for good_link in goods_links_from_page_links:
             all_goods_links.append(good_link)
 
-    data = {'description': [], 'categories': [], 'retailer': [],
-            'dates': [], 'new_price': [], 'old_price': [],
-            'discount': [],  'image': [], 'link': []}
-    database = pd.DataFrame(data=data)
+    database = pd.DataFrame(data=parser.data)
     all_goods_links = [link for link in all_goods_links if (type(link) is not type(None))]
     print(f'Количество ссылок на товары = {len(all_goods_links)}')
+    parser.save_list_to_txt(all_goods_links, "all_goods_links")
 
     all_dict_of_info = []
     counter = Count()
-    for good_link in tqdm(all_goods_links):
+    for good_link in tqdm(all_goods_links[:2]):
         dict_of_info = parser.parse_page(url=good_link)
         all_dict_of_info.append(dict_of_info)
         i = next(counter)
@@ -232,17 +266,11 @@ def main():
             for key, val in dict_of_info.items():
                 database.loc[i, key] = val
 
-    def create_txt():
-        with open("Output.txt", "w") as text_file:
-            for dict_ in all_dict_of_info:
-                for key, val in dict_.items():
-                    text_file.write(val)
-                    text_file.write(';')
-                text_file.write('\n')
-
-    create_txt()
     database.to_excel("Edadil_database.xlsx", sheet_name='Sheet_name_1')
-    database.to_csv('Edadil_database.csv', sep=',', encoding='cp1251')
+    print(f"Database .xlsx created sucsessfully!")
+    database.to_csv('Edadil_database.csv', sep=',')
+    print(f"Database .csv created sucsessfully!")
+    parser.create_databases(database, all_dict_of_info)
     sleep(0.3)
     parser.browser.close()
     parser.browser.quit()
--- read_csv.py
+++ read_csv.py
@@ -2,3 +2,23 @@ import pandas as pd
 
 df = pd.read_csv('Edadil_database.csv')
 print(df.head())
+
+'''
+import requests
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+from urllib.request import urlopen
+
+# a = browser.find_element_by_css_selector("p-offer__quantity")# price-new
+#email = browser.find_element_by_name('ctl00$MainContent$ctlLogin$view')
+#password = browser.find_element_by_name('ctl00$MainContent$ctlLogin$_Password')
+#login = browser.find_element_by_name('ctl00$MainContent$ctlLogin$BtnSubmit')
+# добавление учётных данных для входа
+#email.send_keys('********')
+#password.send_keys('*******')
+# нажатие на кнопку отправки
+#login.click()
+# print(email)
+'''
